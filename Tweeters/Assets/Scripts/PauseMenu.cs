c3ef0c45db8df7c2288ed95e87d173dd1d98e392
Joseph Neubert <joeyfneubert@gmail.com>
2024-04-29 07:19:52 -0700

    New build with 3 levels

--- Tweeters/Assets/Scripts/PauseMenu.cs
+++ Tweeters/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -40,11 +41,16 @@ public class PauseMenu : MonoBehaviour
 
     public void LoadMenu()
     {
-
+        // N/A
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
     }
 }
